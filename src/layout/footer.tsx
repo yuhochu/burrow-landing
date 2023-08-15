47f2a515e931f541a6bccceb07d145ad6a310d7a
Yuho <yuho@ref.finance>
2023-08-15 16:23:15 +0800

    update responsive style

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -3,26 +3,26 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className={'footer -mt-10 md:mt-5 py-10 xl:px-20 lg:px-10 px-6 bg-gray-800'}>
-      <div className={'flex justify-center md:justify-between border-solid md:border-b border-gray-700 md:pb-10 mb-10'}>
-        <div className={'logo hidden md:block'}>{assets.svg.logo}</div>
-        <div className="flex gap-10 md:gap-20 text-gray-300">
-          {NAVIGATIONS.map((d) => {
+    <div className={'footer -mt-10 md:mt-5 py-10 xl:px-20 lg:px-10 px-6 bg-gray-800 flex flex-col md:flex-col-reverse'}>
+      <div className={'flex justify-center md:justify-end mb-5 md:mb-0'}>
+        <div className={'flex gap-5 md:gap-8'}>
+          {ICONS?.map((d) => {
             return (
-              <Link title={d.title} key={d.title} to={d.to}>
-                {d.title}
+              <Link key={d.to} to={d.to} target={d.target} className={'footer-icon'}>
+                {d.svg}
               </Link>
             );
           })}
         </div>
       </div>
 
-      <div className={'flex justify-center md:justify-end'}>
-        <div className={'flex gap-10 md:gap-3'}>
-          {ICONS?.map((d) => {
+      <div className={'flex justify-center md:justify-between border-solid md:border-b border-gray-700 md:pb-10 md:mb-10'}>
+        <div className={'logo hidden md:block'}>{assets.svg.logo}</div>
+        <div className="flex gap-10 md:gap-20 text-gray-300">
+          {NAVIGATIONS.map((d) => {
             return (
-              <Link key={d.to} to={d.to} target={d.target} className={'footer-icon'}>
-                {d.svg}
+              <Link title={d.title} key={d.title} to={d.to}>
+                {d.title}
               </Link>
             );
           })}
@@ -35,15 +35,17 @@ const Footer = () => {
 const NAVIGATIONS = [
   {
     title: 'App',
-    to: '/',
+    to: 'https://app.burrow.cash/',
   },
   {
     title: 'Docs',
     to: '/',
+    target: '_blank',
   },
   {
     title: 'Bug Bounty',
-    to: '/',
+    to: 'https://immunefi.com/bounty/burrow/',
+    target: '_blank',
   },
 ];
 
