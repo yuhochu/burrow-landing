13814e540b6525b9b3cc16db83f76875ca926e96
Yuho <yuho@ref.finance>
2023-10-13 08:40:09 +0800

    fix banner

--- src/pages/homePage/homePageBanner.tsx
+++ src/pages/homePage/homePageBanner.tsx
@@ -18,11 +18,13 @@ const HomePageBanner = () => {
   };
 
   useEffect(() => {
-    console.log("swiperRef.current",swiperRef.current)
-    console.log("brrLottieRef?.current",brrLottieRef?.current)
-    swiperRef?.current?.autoplay.start();
+    console.log('swiperRef.current', swiperRef.current);
+    console.log('brrLottieRef?.current', brrLottieRef?.current);
+    if (!swiperRef?.current?.autoplay?.running) {
+      swiperRef?.current?.autoplay.start();
+    }
     brrLottieRef?.current?.play();
-  }, []);
+  }, [swiperRef?.current]);
 
   const handleSwiperChange = (a: any) => {
     console.log('i', a?.realIndex);
@@ -49,13 +51,13 @@ const HomePageBanner = () => {
       allowTouchMove={false}
       preventInteractionOnTransition={true}
       fadeEffect={{
-        crossFade: true
+        crossFade: true,
       }}
       onSlideChange={handleSwiperChange}
       modules={[EffectFade, Autoplay]}
-      className='animate-swiper'
+      className="animate-swiper"
     >
-      <SwiperSlide data-swiper-autoplay='2720'>
+      <SwiperSlide data-swiper-autoplay="2720">
         <div className={`md:flex mb-10 animate-banner pt-10 md:pt-0`}>
           <div className={`md:flex md:flex-row-reverse`}>
             <div className={`justify-center flex flex-col items-center md:flex-row-reverse`}>
@@ -79,7 +81,7 @@ const HomePageBanner = () => {
         </div>
       </SwiperSlide>
 
-      <SwiperSlide data-swiper-autoplay='5250'>
+      <SwiperSlide data-swiper-autoplay="5250">
         <div className={`md:flex mb-10 animate-banner pt-10 md:pt-0`}>
           <div className={`md:flex md:flex-row-reverse`}>
             <div className={`justify-center flex flex-col items-center md:flex-row-reverse`}>
